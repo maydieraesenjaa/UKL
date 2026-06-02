2056c1037ce0fad7271ee55af5d48b1111511ec9
Maydiera Ardyanto <maydieraevelyn@gmail.com>
2026-06-02 20:51:29 +0700

    sinkronisasi swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { MenuModule } from './menu/menu.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, MenuModule, OrderModule],
+  imports: [PrismaModule, AuthModule, MenuModule, OrderModule, ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath:
+    process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : '.env'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
